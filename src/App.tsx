d065bf212ffdf6e0e9e9329ba9916c292470296f
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2021-03-26 01:46:30 +0100

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import React from 'react';
 import './App.css';
 import Users from './components/Users';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import Landing from './components/Landing';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000',
@@ -11,9 +13,16 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div>
-          <Users />
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/landing" exact>
+            <Landing />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </ApolloProvider>
     
   );
