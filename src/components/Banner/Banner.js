116d5c38d82954ea042f17aceb693daf19ee7178
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2021-07-18 10:16:07 +0100

    ..

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,7 +4,8 @@ import BannerImg from '../../assets/img/resume_for_devops.jpg'
 
 const Banner = () => {
     return (
-        <div className="banner d-flex align-items-center" style={{ backgroundImage: `url(${BannerImg})`}}>
+        <div className="banner d-flex align-items-center position-relative" style={{ backgroundImage: `url(${BannerImg})` }}>
+            <div class="overlay"></div>
             <div className="banner__container app-width">
                 <h2 className="banner__header text-white mt-5">
                     Juniors make IT work.
