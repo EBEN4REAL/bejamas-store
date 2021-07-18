2a2fc802843f8860d8a41e74eee2134b956d72dc
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2021-07-18 09:42:53 +0100

    ..

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import './Banner.css'
+import BannerImg from '../../assets/img/resume_for_devops.jpg'
 
 const Banner = () => {
     return (
-        <div className="banner d-flex align-items-center">
+        <div className="banner d-flex align-items-center" style={{ backgroundImage: `url(${BannerImg})`}}>
             <div className="banner__container app-width">
                 <h2 className="banner__header text-white mt-5">
                     Juniors make IT work.
