f9c0724f042b661e5a1e3add1be6f2b97ec5cf11
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2021-09-24 11:44:12 +0100

    ..

--- src/components/Products/filters.js
+++ src/components/Products/filters.js
@@ -43,7 +43,46 @@ const Filters = () => {
                 </div>
             </div>
             <div className="m-filters">
-                <div className="filter-overlay"></div>
+                <div className="filter-overlay">
+                    <div className="filter-categories">
+                        <div className="app-width">
+                            <div className="category-filters pb-3">
+                                <h4 className="font-bold">Filters</h4>
+                                <div class="checkbox">
+                                    <input type="checkbox" id="checkbox2" name="" value="" />
+                                    <label for="checkbox2"><span>People</span></label>
+                                </div>
+                                <div class="checkbox">
+                                    <input type="checkbox" id="checkbox2" name="" value="" />
+                                    <label for="checkbox2"><span>People</span></label>
+                                </div>
+                                <div class="checkbox">
+                                    <input type="checkbox" id="checkbox2" name="" value="" />
+                                    <label for="checkbox2"><span>People</span></label>
+                                </div>
+                            </div>
+                            <h4 className="font-bold mt-3" > Price range</h4 >
+                            <div className="mt-3">
+                                <div class="checkbox">
+                                    <input type="checkbox" id="checkbox2" name="" value="" />
+                                    <label for="checkbox2"><span>Lower than $20</span></label>
+                                </div>
+                                <div class="checkbox">
+                                    <input type="checkbox" id="checkbox2" name="" value="" />
+                                    <label for="checkbox2"><span>$20 - $100</span></label>
+                                </div>
+                                <div class="checkbox">
+                                    <input type="checkbox" id="checkbox2" name="" value="" />
+                                    <label for="checkbox2"><span>Lower than $20</span></label>
+                                </div>
+                                <div class="checkbox">
+                                    <input type="checkbox" id="checkbox2" name="" value="" />
+                                    <label for="checkbox2"><span>More than $200</span></label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
